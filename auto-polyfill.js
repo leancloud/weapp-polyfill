leancloud/weapp-polyfill

commit 3e76cbe97534691936f83935c91c89618d5803d5

fix: polyfill GameGlobal for weapp game

auto-polyfill.js
@@ -2,8 +2,10 @@ var polyfill = require('./index.js').polyfill;
 try {
   polyfill();
 } catch (e) {}
+try {
+  polyfill(GameGlobal);
+} catch (e) {}
 try{
-  window = window || {};
   polyfill(window);
 } catch (e) {}
 try {

commit b5aad794607341a0cddd067e786d8b0e59baccef

fix: try to assign window with {} again
this fixes a regression introduced in 3e76cbe97534691936f83935c91c89618d5803d5

auto-polyfill.js
@@ -6,6 +6,7 @@ try {
   polyfill(GameGlobal);
 } catch (e) {}
 try{
+  window = window || {};
   polyfill(window);
 } catch (e) {}
 try {